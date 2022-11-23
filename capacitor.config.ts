29d4fc2889c9b7c3d4ceb17dd17248b1f3fb24be
marcos florentin <marcosflorentin1@hotmail.com>
2022-11-23 01:25:32 -0300

    deploy v1.0

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.optirestmozo.app',
+  appName: 'opti-rest.mozo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
